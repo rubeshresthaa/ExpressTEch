08ae21504fe64c37fae1f955a972068f089a26e9
rubeshresthaa <rubesh.shrestha619@gmail.com>
2024-11-22 19:46:54 +0545

    ch

--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,2 +1,5 @@
-export const baseUrl='http://192.168.1.69:5000/api';
-export const imageUrl='http://192.168.1.69:5000';
+export const baseUrl='https://expresstech.onrender.com/api';
+export const imageUrl='https://expresstech.onrender.com';
+
+// export const baseUrl='http://192.168.1.69/api';
+// export const imageUrl='http://192.168.1.69';
