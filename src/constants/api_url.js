71e9bd22e1830b1b0e57a23855264110c02cb3a4
rubeshresthaa <rubesh.shrestha619@gmail.com>
2024-11-24 13:00:24 +0545

    ch

--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,5 +1,10 @@
 // export const baseUrl='https://expresstech.onrender.com/api';
 // export const imageUrl='https://expresstech.onrender.com';
 
-export const baseUrl='http://192.168.1.69:5000/api';
-export const imageUrl='http://192.168.1.69:5000';
+//LocalHost:
+// export const baseUrl='http://192.168.1.69:5000/api';
+// export const imageUrl='http://192.168.1.69:5000';
+
+//Vercel:
+export const baseUrl='https://express-tech-back.vercel.app/api';
+export const imageUrl='https://express-tech-back.vercel.app';
